bf8c817823d881a9716e4522eec07cd75fb363c0
stevenjiang <stevenjiang1110@gmail.com>
2019-01-06 19:15:03 +0800

    加入职介

--- src/Screenable.java
+++ src/Screenable.java
@@ -6,6 +6,7 @@ public interface Screenable {
         if(screen == null) screen = Attributes.byName(s);
         if(screen == null) screen = Enemy.byName(s);
         if(screen == null) screen = Level.byName(s);
+        if(screen == null) screen = Job.byName(s);
         return screen;
     }
 }
